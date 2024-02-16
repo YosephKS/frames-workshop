587c16a3fe99bc27ac6a98321d722e4f1ab04dff
Yoseph Kurnia Soenggoro <yosephkurniasoenggoro@gmail.com>
2024-02-16 19:13:11 +0800

    another fix

--- src/app/api/frames/mint/route.ts
+++ src/app/api/frames/mint/route.ts
@@ -45,6 +45,7 @@ async function getResponse(req: NextRequest) {
             "eip155:7777777:0xe4ceeb0c8dd38c18692a76562343e089febc30ea:32",
         },
       ] as FrameButtonsType,
+      imageAspectRatio: "1:1",
       ogImage: `${process.env.NEXT_PUBLIC_HOSTNAME}/zora.gif`,
       postUrl:
         "https://zora.co/collect/zora:0xa15Bb830aCD9Ab46164e6840E3ef2dBBF9c5E2B3/1",
@@ -59,7 +60,6 @@ async function getResponse(req: NextRequest) {
           label: "Mint for Free",
         },
       ] as FrameButtonsType,
-      imageAspectRatio: "1:1",
       ogImage: `${process.env.NEXT_PUBLIC_HOSTNAME}/zora.gif`,
       postUrl: `${process.env.NEXT_PUBLIC_HOSTNAME}/api/frames/success`,
     };
